eb82c33a4010f322ea33912e84544e44e3c695aa
factionmeet <2576446141@qq.com>
2024-03-12 11:20:48 +0800

    在连接socket后就发送一次数据请求

--- src/hooks/socket.js
+++ src/hooks/socket.js
@@ -11,6 +11,7 @@ export function useSocket(URL) {
         function onConnect() {
             setIsConnected(true);
             socket.on("disconnect", onDisconnect);
+            socket.emit("lastData", "我发送了数据请求");
         }
         function onDisconnect() {
             setIsConnected(false);
@@ -24,15 +25,23 @@ export function useSocket(URL) {
             setHumidity(value.humidity);
             //console.log(value);
         }
+        function onResEvent(value) {
+            const res = JSON.parse(value);
+            //message.success(`我接受到了lastData数据,temp:${res.temperature},hum:${res.humidity}`);
+            setTemperature(res.temperature);
+            setHumidity(res.humidity);
+        }
         socket.on("connect", onConnect);
         socket.on("send", onFooEvent);
         socket.on("connects", (data) => {
             message.success(`${data}`);
         });
+        socket.on("res", onResEvent);
         return () => {
             socket.off('connect', onConnect);
             socket.off('disconnect', onDisconnect);
             socket.off('send', fooEvents);
+            socket.off("res", onResEvent)
         };
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
